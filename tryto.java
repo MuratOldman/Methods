4522869417db527bfc955624a3bb533071881176
MuratOldman <muratkara15102@gmail.com>
2020-04-28 21:55:47 -0400

    first commit

---
+++ tryto.java
@@ -0,0 +1,17 @@
+package Methods;
+
+import java.util.ArrayList;
+
+public class tryto {
+    public static ArrayList<String> test()
+    {
+        ArrayList<String> names = new ArrayList<String>();
+        //write code below
+     names.add("jon");
+        names.add("joni");
+        names.add("jone");
+
+        //leave below code alone
+        return names;
+    }
+}
